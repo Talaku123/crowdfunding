21c7385616336d168b2ff0e3271fe9d1552e2f63
Talaku123 <69880305+Talaku123@users.noreply.github.com>
2024-03-20 02:08:41 -0400

    Add files via upload

---
+++ funding.js
@@ -0,0 +1,17 @@
+
+let count = 0
+
+const btn = document.querySelector(".btn").addEventListener("click", function(){
+
+  count++
+
+  // document.innerHTML = `<h1>${count}</h1>`
+  
+  count.toFixed`<h1>${count}</h1>`
+
+
+})
+
+
+
+
